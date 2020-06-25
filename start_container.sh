ef0f0ae136840c5c3b2a6bc529ffd607db986800
Jun Ando <ando.jun.isis@gmail.com>
2020-06-25 14:03:36 +0900

    Fixed script for maintenance mode.

--- start_container.sh
+++ start_container.sh
@@ -46,13 +46,16 @@ if [[ $name != "maintenance" ]] ; then
   done
 fi
 
+# No need to start container
+if [ $container_exist -eq 1 ] ; then
+  exit
+fi
+
 # Start container
-if [ $container_exist -eq 0 ] ; then
-  echo Info: Start user container: name=$name
+echo Info: Start user container: name=$name
 
-  # Mount user home
-  ls /home/$user > /dev/null
-
+# Clean
+if [[ $name != "maintenance" ]] ; then
   # Clean scratch area
   mkdir -p /scratch
   while [ $(du -s /scratch | awk '{print $1}') -gt $scratch_max_size ] ; do
@@ -61,25 +64,10 @@ if [ $container_exist -eq 0 ] ; then
     rm -r $rmdir
     sleep 1
   done
-
-  # Create scratch area
-  mkdir -p /scratch/$user
-  chown $user /scratch/$user
-  touch /scratch/$user/.start
-
+  
   # Clean
   rm -f /tmp/*.sshd_config
   rm -f /tmp/*.xrdp.ini
-
-  # Create sshd_config
-  sshd_config=$(mktemp --suffix=.sshd_config)
-  cp $cur/sshd_config.base $sshd_config
-  echo AllowUsers $user >> $sshd_config
-
-  # Create xrdp.ini
-  xrdp_ini=$(mktemp --suffix=.xrdp.ini)
-  chmod 644 $xrdp_ini
-  sed "s/%USER%/$user/" $cur/xrdp.ini.base > $xrdp_ini
 
   # Reset FPGA
   /usr/sbin/rmmod xclmgmt
@@ -87,39 +75,57 @@ if [ $container_exist -eq 0 ] ; then
   /usr/sbin/modprobe xclmgmt
   /usr/sbin/modprobe xocl
   yes | /opt/xilinx/xrt/bin/xbutil reset > /dev/null
-
-  # Find driver
-  xocl=$(/opt/xilinx/xrt/bin/xbutil scan | grep "xilinx_u" | sed -r 's/^.*inst=([0-9]*).*/\1/')
-  xocl="/dev/dri/renderD$xocl"
-  if [[ ! -e $xocl ]] ; then
-    echo "Error: can't find xocl"
-    exit 1
-  fi
-  
-  xclmgmt=$(ls -1 /dev/xclmgmt* | head -n 1)
-  if [[ ! -e $xclmgmt ]] ; then
-    echo "Error: can't find xclmgmt"
-    exit 1
-  fi
-
-  docker run \
-    -dit \
-    --network net \
-    --ip $ip \
-    --name "$name" \
-    --hostname $hostname \
-    -v /scratch/$user:/scratch \
-    -v /home/$user:/home/$user \
-    -v /tools:/tools \
-    -v /opt/xilinx/platforms:/opt/xilinx/platforms \
-    -v $sshd_config:/etc/ssh/sshd_config \
-    -v $xrdp_ini:/etc/xrdp/xrdp.ini \
-    --device=$xclmgmt:$xclmgmt \
-    --device=$xocl:$xocl \
-    --cpus=14.000 \
-    --memory 120g \
-    $repo:$tag \
-    $cmd > /dev/null
-
-  echo Info: Started
 fi
+
+# Mount user home
+ls /home/$user > /dev/null
+
+# Create scratch area
+mkdir -p /scratch/$user
+chown $user /scratch/$user
+touch /scratch/$user/.start
+
+# Create sshd_config
+sshd_config=$(mktemp --suffix=.sshd_config)
+cp $cur/sshd_config.base $sshd_config
+echo AllowUsers $user >> $sshd_config
+
+# Create xrdp.ini
+xrdp_ini=$(mktemp --suffix=.xrdp.ini)
+chmod 644 $xrdp_ini
+sed "s/%USER%/$user/" $cur/xrdp.ini.base > $xrdp_ini
+
+# Find driver
+xocl=$(/opt/xilinx/xrt/bin/xbutil scan | grep "xilinx_u" | sed -r 's/^.*inst=([0-9]*).*/\1/')
+xocl="/dev/dri/renderD$xocl"
+if [[ ! -e $xocl ]] ; then
+  echo "Error: can't find xocl"
+  exit 1
+fi
+
+xclmgmt=$(ls -1 /dev/xclmgmt* | head -n 1)
+if [[ ! -e $xclmgmt ]] ; then
+  echo "Error: can't find xclmgmt"
+  exit 1
+fi
+
+docker run \
+  -dit \
+  --network net \
+  --ip $ip \
+  --name "$name" \
+  --hostname $hostname \
+  -v /scratch/$user:/scratch \
+  -v /home/$user:/home/$user \
+  -v /tools:/tools \
+  -v /opt/xilinx/platforms:/opt/xilinx/platforms \
+  -v $sshd_config:/etc/ssh/sshd_config \
+  -v $xrdp_ini:/etc/xrdp/xrdp.ini \
+  --device=$xclmgmt:$xclmgmt \
+  --device=$xocl:$xocl \
+  --cpus=14.000 \
+  --memory 120g \
+  $repo:$tag \
+  $cmd > /dev/null
+
+echo Info: Started
