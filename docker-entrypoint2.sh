2ea1e459ac8b017e7665b59436dea8cd3611a578
Jun Ando <ando.jun.isis@gmail.com>
2020-08-04 01:32:34 +0900

    Creating Dockerfile...

---
+++ docker-entrypoint2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+chown -hR $LOGIN_USER_UID:$LOGIN_USER_GID /opt/vitis_ai/workspace
---
+++ docker/files/vitis_ai/setup.sh
@@ -0,0 +1,4 @@
+export VAI_ROOT=/opt/vitis_ai
+export PYTHONPATH=/opt/vitis_ai/compiler
+. /opt/vitis_ai/conda/etc/profile.d/conda.sh
+conda activate base
