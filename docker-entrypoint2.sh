63ad8f7d640d5ec086ef558b95a43de5d7621223
Jun Ando <juna@xilinx.com>
2020-08-04 16:03:57 +0900

    Fixed entry script

--- docker-entrypoint2.sh
+++ docker-entrypoint2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-chown -hR $LOGIN_USER_UID:$LOGIN_USER_GID /opt/vitis_ai/workspace
-chown -hR $LOGIN_USER_UID:$LOGIN_USER_GID /opt/vitis_ai/CK-TOOLS
-chown -hR $LOGIN_USER_UID:$LOGIN_USER_GID /opt/vitis_ai/conda/bin
+test -e /opt/vitis_ai/workspace && chown -hR $LOGIN_USER_UID:$LOGIN_USER_GID /opt/vitis_ai/workspace
+test -e /opt/vitis_ai/CK-TOOLS  && chown -hR $LOGIN_USER_UID:$LOGIN_USER_GID /opt/vitis_ai/CK-TOOLS
+test -e /opt/vitis_ai/conda/bin && chown -hR $LOGIN_USER_UID:$LOGIN_USER_GID /opt/vitis_ai/conda/bin
