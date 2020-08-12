d08cdf612b4a734ee28d3873d51bb5d13036cb78
Jun Ando <juna@xilinx.com>
2020-08-12 14:59:20 +0900

    Added OLB list script.

---
+++ olb-list.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+SERVER = '172.16.2.5:20080'
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+TIMESPAN = 3
+
+def begin_time(hour)
+  h = (hour / TIMESPAN) * TIMESPAN
+  return format("%02d:%02d:%02d", h, 0, 0)
+end
+
+###########################################################
+# main
+###########################################################
+
+table = {}
+
+t = Time.now
+
+["as001", "as002", "as003", "as004", "ag001"].each do |host|
+  url = "http://#{SERVER}/olb-view.cgi"
+  url += "?acri=acri"
+  url += "&year=#{t.year}"
+  url += "&month=#{t.month}"
+  url += "&date=#{t.day}"
+  url += "&host=#{host}"
+  res = Net::HTTP.get(URI.parse(url))
+  printf "%s ", host
+  puts res
+end
