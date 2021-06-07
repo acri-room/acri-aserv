6830046a5f8d66e44ee346106b90d6293a78fbaf
Jun Ando <juna@xilinx.com>
2021-06-07 18:44:01 +0900

    Added build.sh script.

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+cur=$(dirname $(readlink -f $0))
+host=$(hostname -s)
+
+cd $cur/docker
+
+case $host in
+  aserv1 | gserv1 )
+    echo U200
+    docker build -t acri-as:latest -f Dockerfile .
+    ;;
+  aserv2 )
+    echo U250
+    docker build -t acri-as:latest -f Dockerfile --build-arg PLATFORM="xilinx_u250_xdma_201830_2" .
+    ;;
+  aserv3 )
+    echo U280-ES1
+    docker build -t acri-as:latest -f Dockerfile.u280-es1 .
+    ;;
+  aserv4 )
+    echo U50
+    docker build -t acri-as:latest -f Dockerfile.u50 .
+    ;;
+esac
