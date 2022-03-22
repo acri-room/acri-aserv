1e0d94a3f413da1893be7c8451c99748361d3226
Jun Ando <juna@xilinx.com>
2022-03-22 19:02:37 +0900

    Support Vitis AI 2.0.

---
+++ docker/files/vitis_ai/select-dpu-vck5000.sh
@@ -0,0 +1,14 @@
+dpu=$(echo "DPUCVDX8H
+DPUCVDX8H-DWC" | peco --prompt "Select DPU >" --on-cancel error)
+if [ $? -eq 0 ] ; then
+    echo "$dpu was selected"
+    source $VAI_ROOT/workspace/setup/vck5000/setup.sh $dpu > /dev/null
+    dev=$(/opt/xilinx/xrt/bin/xbutil examine | grep "xilinx_[uv]" | sed -r 's/.*\[([0-9:.]+).*/\1/')
+    echo "Downloading an xclbin to the device..."
+    /opt/xilinx/xrt/bin/xbutil program --user $XLNX_VART_FIRMWARE --device $dev
+    if [[ $dpu == "DPUCVDX8H" ]] ; then
+        export VAI_LIBRARY_MODELS_DIR=/usr/share/vitis_ai_library/models/vck50008pe-DPUCVDX8H
+    elif [[ $dpu == "DPUCVDX8H-DWC" ]] ; then
+        export VAI_LIBRARY_MODELS_DIR=/usr/share/vitis_ai_library/models/vck50006pe-DPUCVDX8H-DWC
+    fi
+fi
