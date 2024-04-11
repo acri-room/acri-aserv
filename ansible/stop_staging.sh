59478a061da318a3f85ac09c01d607da67844f91
Jun Ando <jun.ando@amd.com>
2024-04-11 11:52:36 +0900

    Fixed for lxc v4.

--- ansible/stop_staging.sh
+++ ansible/stop_staging.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
+if [[ $(lxc --version) =~ ^5 ]] ; then
+    publish_opts+=" --reuse "
+fi
+
 lxc stop staging
-lxc publish staging --alias as --compression none --reuse
+lxc publish staging --alias as --compression none $publish_opts
 lxc delete staging
 
